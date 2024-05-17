6f087adedcc6370428b7b4eb1e18dbf671d1fbc2
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-17 10:17:53 -0300

    Deafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("987654321","Iphone 15","Iphone",13);
+Nokia nokia= new Nokia("123456789","Nokia","Nokia",12);
+
+iphone.InstalarAplicativo("Brawl Stars");
+nokia.InstalarAplicativo("Brawl Stars");
